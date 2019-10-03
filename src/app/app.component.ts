935f090b7babfb8160fdd3a31a2b1c1c4096fddf
mark bashir <beejayphil@gmail.com>
2019-10-03 21:47:19 +0100

    added placeholder text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gidi-food';
+  public title:string = 'gidi-food';
+  processDetail:string=`Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+  Vivamus egestas feugiat felis. Mauris lobortis nisl nec mi convallis finibus. 
+  Nulla facilisi. Donec urna orci, placerat vestibulum sapien eget, laoreet egestas elit.`;
+  feedback:string= `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget justo justo.
+   Ut vehicula nisl at nisl finibus, in fermentum dui ultricies. Aenean magna lectus, tempus nec 
+   augue at, laoreet viverra nunc. Nunc efficitur arcu eget nulla hendrerit, sed ultricies risus 
+   pharetra. In tristique pellentesque urna, in rutrum purus imperdiet malesuada. Sed facilisis,
+    eros sit amet dignissim molestie, enim nisi placerat elit, pretium dapibus dui lorem id libero.
+     Quisque quis neque id est tincidunt fringilla. Nunc pretium tellus a sodales elementum.
+      Donec ipsum nisl, bibendum sit amet feugiat quis, porta quis mi. Quisque luctus tortor
+       accumsan ligula pulvinar pellentesque. Maecenas erat orci, imperdiet in leo a, molestie
+        commodo mauris. Sed auctor mi et metus consectetur, ac varius mi placerat. Morbi tincidunt
+  finibus dolor, ac interdum nibh dignissim vel. Nam aliquet tincidunt felis eu convallis. Integer
+  hendrerit facilisis diam, eu congue leo. Sed vestibulum, arcu a rhoncus pulvinar, elit dolor hendrerit dolor,
+  tristique pharetra dolor nulla mattis justo. Duis erat lorem, scelerisque in mauris eu, pretium sagittis sem.
+  Praesent in tortor nisl. Maecenas nulla eros, vulputate sit amet consequat dignissim, finibus in dolor. Fusce sed lectus suscipit est ultricies ullamcorper varius 
+  in turpis. Sed tincidunt, velit vitae vestibulum porta, enim nunc vulputate est, quis egestas mauris mauris sed quam. 
+  Mauris dapibus enim non nisl efficitur commodo. Pellentesque cursus a purus quis hendrerit. Mauris pharetra lacinia auctor.`;
   handleSubmit(event):void{
   event.preventdefault();
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LinkComponent } from './components/link/link.component';
+import { RestuarantCardComponent } from './components/restuarant-card/restuarant-card.component';
+import { ProcessCardComponent } from './components/process-card/process-card.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LinkComponent
+    LinkComponent,
+    RestuarantCardComponent,
+    ProcessCardComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule
